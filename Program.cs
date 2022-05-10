344ee94e72b47297a11baab974ddc62afe2a2f47
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-05-10 16:31:47 -0500

    completed lecture

--- Program.cs
+++ Program.cs
@@ -9,7 +9,12 @@ namespace AbstractClasses
     {
         static void Main(string[] args)
         {
-            
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+
         }
     }
 }
